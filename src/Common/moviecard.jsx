8338505a5c8238be0022567309b90107c9feac21
qvildosolanavaja <qvildosola@asi-dev4.com>
2026-03-06 20:50:14 +0800

    [To do: add update for movie and useEffect]

--- src/Common/moviecard.jsx
+++ src/Common/moviecard.jsx
@@ -1,12 +1,14 @@
 import '../includes/common.css';
 
-const MovieCard = ({ title, genre, onRemove }) => {
+const MovieCard = ({ title, genre, onRemove, onUpdate }) => {
     return (
         <div className="movie-card">
             <div className="movie-card-content">
                 <h3 className="movie-card-title">{title}</h3>
                 <p className="movie-card-year">{genre}</p>
                 <div className="movie-card-actions">
+                    {/* TODO: Add an Edit button that calls onUpdate when clicked.
+                         Only render it if the onUpdate prop is provided (similar to the Remove button below) */}
                     {onRemove && (
                         <button onClick={onRemove} className="btn-remove">
                             Remove
