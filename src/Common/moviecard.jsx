0fd2ea8e0b27fe965646cb8b7bef9da78c00d6fc
luhykie <lykaentera@gmail.com>
2026-03-07 11:21:54 +0800

    Edit MovieCard

--- src/Common/moviecard.jsx
+++ src/Common/moviecard.jsx
@@ -6,14 +6,29 @@ const MovieCard = ({ title, genre, onRemove, onUpdate }) => {
             <div className="movie-card-content">
                 <h3 className="movie-card-title">{title}</h3>
                 <p className="movie-card-year">{genre}</p>
+
                 <div className="movie-card-actions">
-                    {/* TODO: Add an Edit button that calls onUpdate when clicked.
-                         Only render it if the onUpdate prop is provided (similar to the Remove button below) */}
+
+                    {/* Edit Button (only render if onUpdate is provided) */}
+                    {onUpdate && (
+                        <button
+                            onClick={onUpdate}
+                            className="btn-edit"
+                        >
+                            Edit
+                        </button>
+                    )}
+
+                    {/* Remove Button (only render if onRemove is provided) */}
                     {onRemove && (
-                        <button onClick={onRemove} className="btn-remove">
+                        <button
+                            onClick={onRemove}
+                            className="btn-remove"
+                        >
                             Remove
                         </button>
                     )}
+
                 </div>
             </div>
         </div>
