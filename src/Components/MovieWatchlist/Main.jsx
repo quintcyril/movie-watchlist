22ecad8531dab83aafcc0c692eca1da846da578e
Norwejean R. Arnado <n.arnado1011@gmail.com>
2026-03-07 11:09:09 +0800

    [Add a useEffect]

--- src/Components/MovieWatchlist/Main.jsx
+++ src/Components/MovieWatchlist/Main.jsx
@@ -9,6 +9,10 @@ export default function MovieWatchListMain() {
     const [totalCount, setTotalCount] = useState(0);
 
     // TODO: Add a useEffect that recalculates totalCount whenever the movies array changes
+    useEffect (() => {
+        setTotalCount(movies,length);
+    }, [movies]);
+
 
     // TODO: Add two new state variables for editing:
     //   - editingId: tracks which movie is currently being edited (null by default)
