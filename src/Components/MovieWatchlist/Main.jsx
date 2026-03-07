c42df7bb6c745cf93f8dd79b31b5530f8f9c2ed5
SCS\jspec2a23 <owenrobertsulasulamagsayo@gmail.com>
2026-03-06 19:45:13 -0800

    Added a handleUpdateTitle(id) function

--- src/Components/MovieWatchlist/Main.jsx
+++ src/Components/MovieWatchlist/Main.jsx
@@ -41,6 +41,17 @@ export default function MovieWatchListMain() {
     // TODO: Add a handleUpdateTitle(id) function that:
     //   - If editTitle is not empty, updates the matching movie's title in the movies array
     //   - Resets editingId to null and clears editTitle
+    const handleUpdateTitle = (id) => {
+        if (editTitle.trim()) {
+            setMovies(
+                movies.map(movie =>
+                    movie.id === id ? { ...movie, title: editTitle } : movie
+                )
+            );
+        }
+        setEditingId(null);
+        setEditTitle('');
+    }
 
     return (
         <div className="watchlist-container">
