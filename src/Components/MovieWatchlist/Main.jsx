81cbc3fabdce939b194b9aff9e3815d4b834c4b6
Norwejean R. Arnado <n.arnado1011@gmail.com>
2026-03-07 15:47:04 +0800

    Edited the handleUpdateTitle function

--- src/Components/MovieWatchlist/Main.jsx
+++ src/Components/MovieWatchlist/Main.jsx
@@ -42,7 +42,7 @@ export default function MovieWatchListMain() {
     // TODO: Add a handleUpdateTitle(id) function that:
     //   - If editTitle is not empty, updates the matching movie's title in the movies array
     //   - Resets editingId to null and clears editTitle
-    handleUpdateTitle = (id) => {
+    const handleUpdateTitle = (id) => {
         if (editTitle.trim() !== '') {
             setMovies([...movies.map(movie =>
                 movie.id === id ? { ...movie, title: editTitle} : movie
