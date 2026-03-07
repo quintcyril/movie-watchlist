2f6a0d4321ee42ed4538114c296bc1f73ea08a1f
SCS\jspec2a23 <owenrobertsulasulamagsayo@gmail.com>
2026-03-06 20:05:27 -0800

    Added (editingId === movie.id) to check if this movie is being edited

--- src/Components/MovieWatchlist/Main.jsx
+++ src/Components/MovieWatchlist/Main.jsx
@@ -87,11 +87,31 @@ export default function MovieWatchListMain() {
                                - A Save button that calls handleUpdateTitle(movie.id)
                                - A Cancel button that resets editingId to null
                              If no, render the MovieCard below and pass onUpdate to it */}
-                        <MovieCard
+                        {editingId === movie.id ? (
+                            <div>
+                                <input
+                                    type="text"
+                                    value={editTitle}
+                                    onChange={(e) => setEditTitle(e.target.value)}
+                                />
+
+                                <button onClick={() => handleUpdateTitle(movie.id)}>
+                                    Save
+                                </button>
+
+                                <button onClick={() => setEditingId(null)}>
+                                    Cancel
+                                </button>
+                            </div>
+                        ) : (
+                            <MovieCard
                             title={movie.title}
                             genre={movie.genre}
                             onRemove={() => handleRemoveMovie(movie.id)}
-                        />
+                            onUpdate={() => handleStartEdit(movie)}
+                            />
+                        )}
+
                     </div>
                 ))}
             </div>
